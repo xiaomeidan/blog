89488613a4092efc3b699baa5a282302099de689
Amy <327500991@qq.com>
2023-11-02 16:58:58 +0800

    calendar

---
+++ pages/calender/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="box">
+    <h1>{{ year }} 年 {{ month + 1 }} 月 {{ day }} 日</h1>
+    <h1>星期{{ weekMap[week].ch }}</h1>
+    <h1>{{ weekMap[week].en }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+
+const weekMap = {
+  0: { ch: "一", en: "Monday" },
+  1: { ch: "二", en: "Tuesday" },
+  2: { ch: "三", en: "Wednesday" },
+  3: { ch: "四", en: "Thursday" },
+  4: { ch: "五", en: "Friday" },
+  5: { ch: "六", en: "Saturday" },
+  6: { ch: "七", en: "Sunday" },
+};
+
+const date = new Date();
+const year = date.getFullYear();
+const month = date.getMonth();
+const day = date.getDate();
+const week = date.getDay();
+</script>
+
+<style scoped>
+.box {
+  display: flex;
+  text-align: center;
+  flex-direction: column;
+}
+</style>
