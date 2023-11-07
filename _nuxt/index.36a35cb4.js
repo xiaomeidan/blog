e6bf14234f256f72c619ca7658f3dead264ed0da
Amy <327500991@qq.com>
2023-11-07 13:44:51 +0800

    b170464

---
+++ _nuxt/index.36a35cb4.js
@@ -0,0 +1,1 @@
+import{d as n,J as o,s,t as e,v as a,K as r,q as u}from"./entry.eca5fd71.js";const l=e("h1",null,"Learn Nuxt folder",-1),i=n({__name:"index",setup(c){const t=o();return(p,_)=>(u(),s("div",null,[l,e("p",null,"Current route: "+a(r(t).path),1)]))}});export{i as default};
