1eb89b4eb6b99d2918ca8bdfb7fd7221b0d6374e
Amy <327500991@qq.com>
2023-11-09 17:33:54 +0800

    ad

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,25 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import VueSetupExtend from 'vite-plugin-vue-setup-extend';
+import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    VueSetupExtend(),
+    AutoImport({
+      dts: 'src/auto-imports.d.ts',
+      imports: ['vue']
+    })
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      src: fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+});
